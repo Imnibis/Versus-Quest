5c8c11149c69aeb060cc82011d4113ef86dedddb
Imnibis <rayane.djellabi@epitech.eu>
2021-11-07 14:06:27 +0100

    ShooterProjectile now kills stuff

--- enemies/shooter/ShooterProjectile.gd
+++ enemies/shooter/ShooterProjectile.gd
@@ -10,13 +10,21 @@ var speed
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body
+	connect("body_entered", self, "_on_ShooterProjectile_body_entered")
+	$Timeout.connect("timeout", self, "timeout")
 
 func _physics_process(delta):
 	position.x += (speed * delta) * (-1 if direction == "LEFT" else 1)
 	$AnimatedSprite.flip_h = (direction == "RIGHT")
 	$LeftCollision.disabled = (direction == "RIGHT")
 	$RightCollision.disabled = (direction == "LEFT")
+
+func _on_ShooterProjectile_body_entered(body):
+	if body.is_in_group("playable") and body != shooter:
+		queue_free()
+
+func timeout():
+	queue_free()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
