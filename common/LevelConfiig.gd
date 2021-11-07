4e723afdf3eb272afe9846d81e31a9d7713ec7cc
Imnibis <rayane.djellabi@epitech.eu>
2021-11-07 17:47:31 +0100

    Done defeat and victory screens (but really this time)

--- common/LevelConfiig.gd
+++ common/LevelConfiig.gd
@@ -8,7 +8,7 @@ func _ready():
 	match nodeName:
 		"Level-3":
 			levelIfWin = "res://scenes/level -2/level-2.tscn"
-			levelIfLose = "res://scenes/level0/level0.tscn"
+			levelIfLose = "res://menus/Defeat.tscn"
 		"Level-2":
 			levelIfWin = "res://scenes/level -1/level-1.tscn"
 			levelIfLose = "res://scenes/level -3/level-3.tscn" 
@@ -25,7 +25,7 @@ func _ready():
 			levelIfWin = "res://scenes/level3/level3.tscn"
 			levelIfLose = "res://scenes/level1/level1.tscn"
 		"Level3":
-			levelIfWin = "res://scenes/level0/level0.tscn"
+			levelIfWin = "res://menus/Victory.tscn"
 			levelIfLose = "res://scenes/level2/level2.tscn"
 	$Player.connect("win", self, "player_won")
 	$Player.connect("dead", self, "player_dead")
